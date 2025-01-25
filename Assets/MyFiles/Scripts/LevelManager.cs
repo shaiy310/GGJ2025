6ac008f9df22f77db8b53870de990eb1cdaed90c
shaiy310 <shaiy310@gmail.com>
2025-01-25 14:41:32 +0200

    tab -> space

--- Assets/MyFiles/Scripts/LevelManager.cs
+++ Assets/MyFiles/Scripts/LevelManager.cs
@@ -8,42 +8,42 @@ using UnityEngine.UI;
 
 namespace MyFiles.Scripts
 {
-	[Serializable]
-	struct Level
-	{
-		public string Name;
-		public float time;
-		public int scoreThreshold;
-		public GameObject map;
-	}
+    [Serializable]
+    struct Level
+    {
+        public string Name;
+        public float time;
+        public int scoreThreshold;
+        public GameObject map;
+    }
 
-	public class LevelManager : MonoBehaviour
-	{
-		public static LevelManager Instance { get; private set; }
+    public class LevelManager : MonoBehaviour
+    {
+        public static LevelManager Instance { get; private set; }
 
-		[SerializeField] Timer timer;
-		[SerializeField] Level[] levels;
-		[SerializeField] TextMeshProUGUI levelNameTxt;
-		[SerializeField] TextMeshProUGUI thresholdTxt;
-		int score = 0; // TBD
-		
-		public int Level { get; private set; }
-		public float MinX => _bounds.min.x - _bounds.center.x;
-		public float MaxX => _bounds.max.x - _bounds.center.x;
+        [SerializeField] Timer timer;
+        [SerializeField] Level[] levels;
+        [SerializeField] TextMeshProUGUI levelNameTxt;
+        [SerializeField] TextMeshProUGUI thresholdTxt;
+        int score = 0; // TBD
+        
+        public int Level { get; private set; }
+        public float MinX => _bounds.min.x - _bounds.center.x;
+        public float MaxX => _bounds.max.x - _bounds.center.x;
 
-		private Bounds _bounds;
+        private Bounds _bounds;
 
-		[SerializeField] private VideoActivator _videoActivator;
-		[SerializeField] private Image _fadeImage;
-		[SerializeField] private float _fadeInTime = 0.2f; 
-		[SerializeField] private float _fadeOutTime = 0.3f;
-		[SerializeField] private AudioEvent _levelTransitionAudioEvent;
-		[SerializeField] private AudioEvent _levelWinAudioEvent;
+        [SerializeField] private VideoActivator _videoActivator;
+        [SerializeField] private Image _fadeImage;
+        [SerializeField] private float _fadeInTime = 0.2f; 
+        [SerializeField] private float _fadeOutTime = 0.3f;
+        [SerializeField] private AudioEvent _levelTransitionAudioEvent;
+        [SerializeField] private AudioEvent _levelWinAudioEvent;
 
-		private void Awake()
-		{
-			Instance = this;
-		}
+        private void Awake()
+        {
+            Instance = this;
+        }
 
         private void OnEnable()
         {
@@ -58,111 +58,111 @@ namespace MyFiles.Scripts
         private void OnScoreChanged(ScoreChangedEvent scoreChangedEvent)
         {
             score = scoreChangedEvent.Score;
-			if (score == levels[Level].scoreThreshold) {
-				OnEndLevel();
+            if (score == levels[Level].scoreThreshold) {
+                OnEndLevel();
             }
         }
 
         private void Start()
-		{
+        {
             timer.OnTimerEnd += OnEndLevel;
-			LoadLevel(0);
-		}
+            LoadLevel(0);
+        }
 
         private void LoadLevel(int level)
         {
-	        StartCoroutine(LoadLevelEnumerator(level));
+            StartCoroutine(LoadLevelEnumerator(level));
         }
 
         private IEnumerator LoadLevelEnumerator(int level)
         {
-	        //fade in
-	        _videoActivator.PlayVideo();
-	        _levelTransitionAudioEvent.Play();
-	        if (level != 0)
-	        {
-		        yield return StartCoroutine(FadeEnumerator(0, 1, _fadeInTime));
-	        }
-	        else
-	        {
-		        yield return StartCoroutine(FadeEnumerator(1, 1, _fadeInTime));
-	        }
+            //fade in
+            _videoActivator.PlayVideo();
+            _levelTransitionAudioEvent.Play();
+            if (level != 0)
+            {
+                yield return StartCoroutine(FadeEnumerator(0, 1, _fadeInTime));
+            }
+            else
+            {
+                yield return StartCoroutine(FadeEnumerator(1, 1, _fadeInTime));
+            }
 
-	        //load the actual level
-	        Level = level;
-	        for (int i = 0; i < levels.Length; i++) {
-		        levels[i].map.SetActive(i == level);
-	        }
-	        SetBounds();
-	        levelNameTxt.text = levels[Level].Name;
-			thresholdTxt.text = $"/{levels[Level].scoreThreshold}";
+            //load the actual level
+            Level = level;
+            for (int i = 0; i < levels.Length; i++) {
+                levels[i].map.SetActive(i == level);
+            }
+            SetBounds();
+            levelNameTxt.text = levels[Level].Name;
+            thresholdTxt.text = $"/{levels[Level].scoreThreshold}";
 
             timer.StartTimer(levels[Level].time);
-	        Messenger.Default.Publish(new NewLevelEvent());
-	        
-	        //fade out
-	        yield return StartCoroutine(FadeEnumerator(1,0,_fadeOutTime));
+            Messenger.Default.Publish(new NewLevelEvent());
+            
+            //fade out
+            yield return StartCoroutine(FadeEnumerator(1,0,_fadeOutTime));
         }
 
         private IEnumerator FadeEnumerator(float from, float to, float time)
         {
-	        var startTime = Time.time;
-	        var endTime = startTime + time;
-	        while (Time.time < endTime)
-	        {
-		        var t = 1 - (endTime - Time.time) / time;
-		        var color = _fadeImage.color;
-		        color.a = Mathf.Lerp(from, to, t);
-		        _fadeImage.color = color;
-		        yield return null;
-	        }
-	        var color2 = _fadeImage.color;
-	        color2.a = to;
-	        _fadeImage.color = color2;
+            var startTime = Time.time;
+            var endTime = startTime + time;
+            while (Time.time < endTime)
+            {
+                var t = 1 - (endTime - Time.time) / time;
+                var color = _fadeImage.color;
+                color.a = Mathf.Lerp(from, to, t);
+                _fadeImage.color = color;
+                yield return null;
+            }
+            var color2 = _fadeImage.color;
+            color2.a = to;
+            _fadeImage.color = color2;
         }
 
         private void OnEndLevel()
         {
             // check score
-			if (score >= levels[Level].scoreThreshold)
-			{
-				OnLevelWin();
-			} else {
-				LoadLevel(Level);
-			}
+            if (score >= levels[Level].scoreThreshold)
+            {
+                OnLevelWin();
+            } else {
+                LoadLevel(Level);
+            }
         }
 
         private void OnLevelWin()
         {
-	        StartCoroutine(OnLevelWinEnumerator());
+            StartCoroutine(OnLevelWinEnumerator());
         }
 
         private IEnumerator OnLevelWinEnumerator()
         {
-	        _levelWinAudioEvent.Play();
+            _levelWinAudioEvent.Play();
 
-	        yield return new WaitForSeconds(2);
-	        
-	        if (Level + 1 < levels.Length) {
-		        LoadLevel(Level + 1);
-	        } else {
-		        // end all levels
-		        Debug.Log("finished all levels");
-	        }
+            yield return new WaitForSeconds(2);
+            
+            if (Level + 1 < levels.Length) {
+                LoadLevel(Level + 1);
+            } else {
+                // end all levels
+                Debug.Log("finished all levels");
+            }
         }
 
         private void SetBounds()
-		{
-			_bounds = new Bounds();
-			var obstacleMovers = levels[Level].map.GetComponentsInChildren<ObstacleMover>();
-			foreach (var obstacleMover in obstacleMovers)
-			{
-				var spriteRenderers = obstacleMover.GetComponentsInChildren<SpriteRenderer>();
-				foreach (var spriteRenderer in spriteRenderers)
-				{
-					_bounds.Encapsulate(spriteRenderer.bounds);
-				}
-			}
-		}
-	}
+        {
+            _bounds = new Bounds();
+            var obstacleMovers = levels[Level].map.GetComponentsInChildren<ObstacleMover>();
+            foreach (var obstacleMover in obstacleMovers)
+            {
+                var spriteRenderers = obstacleMover.GetComponentsInChildren<SpriteRenderer>();
+                foreach (var spriteRenderer in spriteRenderers)
+                {
+                    _bounds.Encapsulate(spriteRenderer.bounds);
+                }
+            }
+        }
+    }
 }
