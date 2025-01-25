013c0c9c19db92c0b1a72351360f83a516b1fb83
shaiy310 <shaiy310@gmail.com>
2025-01-25 13:23:23 +0200

    Added fading to tutorial messages

---
+++ Assets/TutorialAnimator.cs
@@ -0,0 +1,33 @@
+using MyFiles.Scripts.Events;
+using SuperMaxim.Messaging;
+using UnityEngine;
+
+public class TutorialAnimator : MonoBehaviour
+{
+    Animation anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animation>();
+    }
+
+    private void OnEnable()
+    {
+        Messenger.Default.Subscribe<PushDownEvent>(RunAnimation);
+    }
+
+    private void OnDisable()
+    {
+        Messenger.Default.Unsubscribe<PushDownEvent>(RunAnimation);
+    }
+    
+    void RunAnimation(PushDownEvent pushDownEvent)
+    {
+        if (pushDownEvent.IsPressed) {
+            anim.Play("TutorialFadeOut");
+        } else {
+            anim.Play("TutorialFadeIn");
+        }
+    }
+
+}
